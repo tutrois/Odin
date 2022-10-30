45ae342ddd3eb1391282705287c8118ff81267eb
Arthur Ramos <arthur.ramos@audaztec.com.br>
2022-10-30 17:12:03 -0300

    Configuração do Ticket

---
+++ Odin/Service/Ticket/ITicketService.cs
@@ -0,0 +1,9 @@
+﻿using Odin.Data.Entities.Models;
+
+namespace Odin.Service
+{
+    public interface ITicketService
+    {
+        Task<List<TicketModel>> GetAll();
+    }
+}
---
+++ Odin/Service/Ticket/TicketService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Odin.Data.Database;
+using Odin.Data.Entities.Models;
+
+namespace Odin.Service
+{
+    public class TicketService : ITicketService
+    {
+        private readonly OdinDBContex _dbContex;
+
+        public TicketService(OdinDBContex odinDBContex)
+        {
+            _dbContex = odinDBContex;
+        }
+
+        public async Task<List<TicketModel>> GetAll()
+        {
+            return await _dbContex.Tickets.OrderBy(e => e.CreatedOn).ToListAsync();
+        }
+    }
+}
